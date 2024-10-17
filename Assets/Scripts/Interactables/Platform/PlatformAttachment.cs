35021981ca747320187f600a3ad99f5cf8058d64
이인환 <70061182+leeinhwan0421@users.noreply.github.com>
2024-10-18 02:54:29 +0900

    update some

--- Assets/Scripts/Interactables/Platform/PlatformAttachment.cs
+++ Assets/Scripts/Interactables/Platform/PlatformAttachment.cs
@@ -4,33 +4,13 @@ using UnityEngine;
 
 public class PlatformAttachment : InteractableCollision
 {
-    private HashSet<GameObject> colls = new HashSet<GameObject>();
-    private Vector3 prev;
-
-    private void Start()
-    {
-        prev = transform.position;
-    }
-
-    private void Update()
-    {
-        Vector3 cur = transform.position;
-        
-        foreach(GameObject item in colls)
-        {
-            item.transform.position += (cur - prev);
-        } 
-
-        prev = cur;
-    }
-
     protected override void EnterEvent(Collision2D collision)
     {
-        colls.Add(collision.gameObject);
+        collision.transform.parent = transform;
     }
 
     protected override void ExitEvent(Collision2D collision)
     {
-        colls.Remove(collision.gameObject);
+        collision.transform.parent = null;
     }
 }
