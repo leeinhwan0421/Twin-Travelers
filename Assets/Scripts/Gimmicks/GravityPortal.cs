11f05981dc73d29179a0e14105575c6ac786ee45
leeinhwan0421 <70061182+leeinhwan0421@users.noreply.github.com>
2024-10-21 17:52:00 +0900

    2024 10 21

---
+++ Assets/Scripts/Gimmicks/GravityPortal.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityPortal : InteractableTrigger
+{
+    protected override void EnterEvent(Collider2D collision)
+    {
+    }
+
+    protected override void ExitEvent(Collider2D collision)
+    {
+        // Nothing
+    }
+}
