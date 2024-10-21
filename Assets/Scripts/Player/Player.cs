baf352a4865e82aa408a802a55aa0e5d7f4f1748
이인환 <70061182+leeinhwan0421@users.noreply.github.com>
2024-10-21 21:19:07 +0900

    update some

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,6 +5,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
@@ -42,8 +43,11 @@ public class Player : MonoBehaviour
     private Vector3 moveDirection;
 
     private float checkGroundDistance = 0.1f;
-    private bool isGrounded;
 
+    private bool isGrounded;
+    private GravityPortal.GravityPortalType gravityType;
+
+    #region Unity LifeCycle
     private void Awake()
     {
         animator = GetComponent<Animator>();
@@ -55,18 +59,17 @@ public class Player : MonoBehaviour
         {
             GetComponent<PlayerInput>().SwitchCurrentControlScheme(new[] { Keyboard.current });
         }
+
+        ChangeGravity(GravityPortal.GravityPortalType.Down);
     }
 
     private void Update()
     {
         SetAnimatorParameters();
-    }
-
-    private void FixedUpdate()
-    {
         Move();
         OnGrounded();
     }
+    #endregion
 
     #region PlayerInput Event Handle
     private void OnMove(InputValue value)
@@ -82,33 +85,51 @@ public class Player : MonoBehaviour
 
         AudioManager.Instance.PlaySFX("Jump");
 
-        rigid.velocity = Vector2.up * JumpVelocity;
+        rigid.velocity = transform.up.normalized * JumpVelocity;
     }
     #endregion
 
-    // 이동 및 여러가지 함수 
+    /// <summary>
+    /// 이동 및 여러가지 메서드 
+    /// </summary>
     private void Move()
     {
         if (moveDirection == Vector3.zero)
             return;
 
-        transform.Translate(moveDirection * Time.fixedDeltaTime * moveSpeed);
+        transform.Translate(moveDirection * Time.deltaTime * moveSpeed);
         spriteRenderer.flipX = moveDirection.x < 0;
     }
 
-    // 땅에 닿았는지를 업데이트하는 함수
+    /// <summary>
+    /// 땅에 닿았는지를 업데이트하는 메서드
+    /// </summary>
     private void OnGrounded()
     {
-        // 모서리에 걸쳐있을 때도 점프가 가능하도록 하기 위해서, bounds.min.x, bounds.max.x 위치에서도 동일하게 작업해줍니다.
-        Vector2[] list = new Vector2[] {
-            new Vector2(coll.bounds.min.x, coll.bounds.min.y),
-            new Vector2(transform.position.x, coll.bounds.min.y),
-            new Vector2(coll.bounds.max.x, coll.bounds.min.y),
-        };
+        Vector2[] list = new Vector2[0];
 
-        foreach(Vector2 v in list)
+        if (gravityType == GravityPortal.GravityPortalType.Up)
         {
-            RaycastHit2D hit = Physics2D.Raycast(v, Vector3.down, checkGroundDistance, LayerMask.GetMask("Ground"));
+            list = new Vector2[]
+            {
+                new Vector2(coll.bounds.min.x, coll.bounds.max.y),
+                new Vector2(transform.position.x, coll.bounds.max.y),
+                new Vector2(coll.bounds.max.x, coll.bounds.max.y),
+            };
+        }
+        else if (gravityType == GravityPortal.GravityPortalType.Down)
+        {
+            list = new Vector2[]
+            {
+                new Vector2(coll.bounds.min.x, coll.bounds.min.y),
+                new Vector2(transform.position.x, coll.bounds.min.y),
+                new Vector2(coll.bounds.max.x, coll.bounds.min.y),
+            };
+        }
+
+        foreach (Vector2 v in list)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(v, -transform.up, checkGroundDistance, LayerMask.GetMask("Ground"));
 
             if (hit.collider != null)
             {
@@ -122,7 +143,9 @@ public class Player : MonoBehaviour
         }
     }
 
-    // 애니메이터 설정
+    /// <summary>
+    /// 애니메이터 설정 메서드
+    /// </summary>
     private void SetAnimatorParameters()
     {
         animator.SetBool("IsMove", moveDirection.x != 0);
@@ -130,8 +153,44 @@ public class Player : MonoBehaviour
         animator.SetFloat("Velocity", rigid.velocity.y);
     }
 
+    /// <summary>
+    /// 사망 이펙트 초기화 및 생성 메서드
+    /// </summary>
     public void InstantiateDeadEffect()
     {
         GameObject.Instantiate(deadEffect, transform.position, Quaternion.identity);
+    }
+
+    /// <summary>
+    /// 중력 타입을 지정하는 메서드
+    /// </summary>
+    /// <param name="type">중력 타입 지정</param>
+    public void ChangeGravity(GravityPortal.GravityPortalType type)
+    {
+        gravityType = type;
+
+        rigid.velocity = new Vector2(Math.Clamp(rigid.velocity.x, -9.8f, 9.8f),
+                                     Math.Clamp(rigid.velocity.y, -9.8f, 9.8f));
+
+        switch (type)
+        {
+            case GravityPortal.GravityPortalType.Up:
+
+                rigid.gravityScale = -Math.Abs(rigid.gravityScale);
+                transform.rotation = Quaternion.Euler(180.0f, 0.0f, 0.0f);
+
+                break;
+            case GravityPortal.GravityPortalType.Down:
+
+                rigid.gravityScale = Math.Abs(rigid.gravityScale);
+                transform.rotation = Quaternion.identity;
+
+                break;
+            default:
+#if SHOW_DEBUG_MESSAGES
+                Debug.LogWarning($"call with GravityPortalType null reference");
+#endif
+                break;
+        }
     }
 }
