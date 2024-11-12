699be847a58c4f96a4ea7a85cd940940b228aeae
leeinhwan0421 <70061182+leeinhwan0421@users.noreply.github.com>
2024-11-12 17:20:57 +0900

    2024.11.12
    ---
    
    - Theme 5 Stage 7 추가
    - Theme 5 Stage 8 추가
    - Theme 5 Stage 9 추가
    - Theme 5 Stage 10 추가
    
    - 게임 종료 버튼 제작
    
    - 게임 시작하자마자 해상도 세팅 변경되도록 변경 (다른 씬에서도 변경되게 하려면 Resources/ResolutionManager 프리펩 추가하면 됨.)
    
    - 폴더/파일 정리

--- Assets/Scripts/UI/Button/LoadSceneButton.cs
+++ Assets/Scripts/UI/Button/LoadSceneButton.cs
@@ -27,4 +27,9 @@ public class LoadSceneButton : MonoBehaviour
             LoadSceneManager.LoadScene("TitleScene");
         }
     }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 }
--- Assets/Scripts/UI/Settings/ResolutionSettings.cs
+++ Assets/Scripts/UI/Settings/ResolutionSettings.cs
@@ -18,72 +18,44 @@ public class ResolutionSettings : MonoBehaviour
         InitializeFullscreenToggle();
     }
 
-    #region Resolution
     private void InitializeResolutionDropdown()
     {
         resolutionDropdown.options.Clear();
 
-        List<Resolution> resolutions = Screen.resolutions
-            .Where(res => Mathf.Approximately((float)res.width / res.height, 16f / 9f))
-            .Reverse()
-            .ToList();
+        List<Resolution> resolutions = ResolutionManager.Instance.resolutions;
 
         for (int i = 0; i < resolutions.Count; i++)
         {
             Resolution res = resolutions[i];
 
-            if (Mathf.Approximately((float)res.width / res.height, 16f / 9f))
-            {
-                float refreshRate = (float)res.refreshRateRatio.numerator / res.refreshRateRatio.denominator;
-                string option = $"{res.width} x {res.height} {refreshRate:F1}Hz ";
-                resolutionDropdown.options.Add(new TMP_Dropdown.OptionData(option));
-            }
-
+            float refreshRate = (float)res.refreshRateRatio.numerator / res.refreshRateRatio.denominator;
+            string option = $"{res.width} x {res.height} {refreshRate:F1}Hz ";
+            resolutionDropdown.options.Add(new TMP_Dropdown.OptionData(option));
         }
 
         resolutionDropdown.onValueChanged.RemoveAllListeners();
-        resolutionDropdown.onValueChanged.AddListener(delegate { OnResolutionChange(resolutions[resolutionDropdown.value], resolutionDropdown.value); });
+        resolutionDropdown.onValueChanged.AddListener(delegate { ResolutionManager.OnResolutionChange(resolutions[resolutionDropdown.value], resolutionDropdown.value); });
 
         if (SettingManager.ResolutionIndex >= 0 && SettingManager.ResolutionIndex < resolutions.Count)
         {
-            OnResolutionChange(resolutions[SettingManager.ResolutionIndex], SettingManager.ResolutionIndex);
+            ResolutionManager.OnResolutionChange(resolutions[SettingManager.ResolutionIndex], SettingManager.ResolutionIndex);
             resolutionDropdown.value = SettingManager.ResolutionIndex;
         }
         else
         {
-            OnResolutionChange(resolutions[0], 0);
+            ResolutionManager.OnResolutionChange(resolutions[0], 0);
             resolutionDropdown.value = 0;
         }
 
         resolutionDropdown.RefreshShownValue();
     }
 
-    public void OnResolutionChange(Resolution selectedResolution, int resolutionIndex)
-    {
-        Screen.SetResolution(selectedResolution.width, selectedResolution.height, Screen.fullScreenMode, selectedResolution.refreshRateRatio);
-        SettingManager.SetResolutionIndex(resolutionIndex);
-
-#if SHOW_DEBUG_MESSAGES
-        Debug.Log($"Resolution set to: {selectedResolution.width} x {selectedResolution.height} @ {selectedResolution.refreshRateRatio.numerator / selectedResolution.refreshRateRatio.denominator}Hz");
-#endif
-    }
-    #endregion
-
-    #region FullScreen
     private void InitializeFullscreenToggle()
     {
         fullscreenToggle.onValueChanged.RemoveAllListeners();
         fullscreenToggle.isOn = SettingManager.IsFullScreen;
-        fullscreenToggle.onValueChanged.AddListener(delegate { OnFullScreenChange(fullscreenToggle.isOn); });
+        fullscreenToggle.onValueChanged.AddListener(delegate { ResolutionManager.OnFullScreenChange(fullscreenToggle.isOn); });
 
-        OnFullScreenChange(SettingManager.IsFullScreen);
+        ResolutionManager.OnFullScreenChange(SettingManager.IsFullScreen);
     }
-
-    private void OnFullScreenChange(bool isOn)
-    {
-        Screen.fullScreenMode = isOn ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
-        SettingManager.SetIsFullScreen(isOn);
-    }
-
-    #endregion
 }
