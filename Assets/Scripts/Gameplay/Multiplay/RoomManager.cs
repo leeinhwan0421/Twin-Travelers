6241c485e8d568de7e1bd2545ce1a0b13b0522d5
leeinhwan0421 <70061182+leeinhwan0421@users.noreply.github.com>
2024-11-20 10:37:26 +0900

    2024.11.20
    - 여러 UI 추가 ( 같이 하기 메뉴 등 )
    - PUN 2 사용해서 멀티 플레이 연결 만들 예정
    
    - RoomManager 추가

---
+++ Assets/Scripts/Gameplay/Multiplay/RoomManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Realtime;
+using Photon.Pun;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+
+}
