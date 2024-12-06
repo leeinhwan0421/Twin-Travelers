d091a1faea271bdbe8ba421473b8535c7a5051e3
leeinhwan0421 <70061182+leeinhwan0421@users.noreply.github.com>
2024-12-06 17:19:37 +0900

    2024.12.06
    - Moveable.TeleportIfDistanceGreaterThan 수정
    
    - 멀티플레이일 경우, 플레이 상태 → 방 나가기 버튼 추가
    
    - README 수정

--- Assets/Scripts/UI/Panel/TitleScene/StatusPanel.cs
+++ Assets/Scripts/UI/Panel/TitleScene/StatusPanel.cs
@@ -12,6 +12,7 @@ public class StatusPanel : Panel
     [SerializeField] private string multiText;
     [Space(10.0f)]
     [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private GameObject button;
 
     public new void Enable()
     {
@@ -20,14 +21,22 @@ public class StatusPanel : Panel
         switch (RoomManager.Instance.playmode)
         {
             case RoomManager.Playmode.None: // 에러
+                button.SetActive(false);
                 text.text = errorText; 
                 break;
             case RoomManager.Playmode.Single: // 오프라인이거나 싱글 플레이
-                text.text = singleText; 
+                button.SetActive(false);
+                text.text = singleText;
                 break;
             case RoomManager.Playmode.Multi: // 멀티 플레이 중
+                button.SetActive(true);
                 text.text = $"{multiText}\n게임 번호: {PhotonNetwork.CurrentRoom.Name}";
                 break;
         }
+    }
+
+    public void LeaveRoom()
+    {
+        RoomManager.Instance.LeaveRoom();
     }
 }
